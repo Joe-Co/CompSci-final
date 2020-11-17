f9435d00462ef31504b6d1ebb82917fb782ffaf2
Joseph <tusjoe@gmail.com>
2020-11-17 15:51:56 -0700

    Trying to get code from eclipse to github

---
+++ WizardGame/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//put the stuff in here
+		System.out.println("testing testing 123");
+		System.out.println("something something somethting");
+	}
+
+}
